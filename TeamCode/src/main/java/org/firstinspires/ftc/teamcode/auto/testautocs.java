56bd6bd4c8d931badac66ff61697815796218805
milesmuehlbach <muehlbachmiles@gmail.com>
2025-09-07 11:02:26 -0500

    add pp and rr and some test opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/testautocs.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+
+@Autonomous(name = "Miles' Test auto (Centerstage)")
+public class testautocs extends LinearOpMode{
+
+
+    Limelight3A limelight;
+
+    public void runOpMode(){
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(60); // This sets how often we ask Limelight for data (100 times per second)
+        waitForStart();
+        limelight.start();
+        limelight.pipelineSwitch(0);
+        telemetry.addLine("Telemetry works!");
+        while(opModeIsActive()) { // Changed from while(true)
+            LLResult result = limelight.getLatestResult();
+            if (result != null && result.isValid()) {
+                double tx = result.getTx(); // How far left or right the target is (degrees)
+                double ty = result.getTy(); // How far up or down the target is (degrees)
+                double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+
+                telemetry.addData("Target X", tx);
+                telemetry.addData("Target Y", ty);
+                telemetry.addData("Target Area", ta);
+            } else {
+                telemetry.addData("Limelight", "No Targets");
+            }
+            telemetry.update();
+        }
+    }
+}
