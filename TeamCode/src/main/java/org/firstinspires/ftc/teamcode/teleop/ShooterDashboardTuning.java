11beb8fadfa8c10f7971a7cd63d429abcc977863
Derek Townsend <derekravioli@gmail.com>
2025-11-13 19:16:48 -0600

    Metele?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ShooterDashboardTuning.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.limelightvision.LLResult;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.util.ShooterPIDConfig;
+import org.firstinspires.ftc.teamcode.util.ll;
+
+@TeleOp(name = "Shooter Dashboard Tuning", group = "Tuning")
+public class ShooterDashboardTuning extends LinearOpMode {
+
+    private DcMotorEx thrower1, thrower2;
+
+    public static double hoodPos = 0.25;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        thrower1 = hardwareMap.get(DcMotorEx.class, "thrower1");
+        thrower2 = hardwareMap.get(DcMotorEx.class, "thrower2");
+
+        thrower1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        thrower2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        thrower1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        thrower2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        Servo hood = hardwareMap.servo.get("hood");
+
+        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // Apply live Dashboard PIDF values
+            thrower1.setVelocityPIDFCoefficients(
+                    ShooterPIDConfig.kP,
+                    ShooterPIDConfig.kI,
+                    ShooterPIDConfig.kD,
+                    ShooterPIDConfig.kF
+            );
+
+            thrower2.setVelocityPIDFCoefficients(
+                    ShooterPIDConfig.kP,
+                    ShooterPIDConfig.kI,
+                    ShooterPIDConfig.kD,
+                    ShooterPIDConfig.kF
+            );
+
+//            double targetTps = ShooterPIDConfig.targetRpm * ShooterPIDConfig.TICKS_PER_REV / 60.0;
+            double targetTps = ll.fetchFlywheelSpeed(limelight) * ShooterPIDConfig.TICKS_PER_REV / 60.0;
+
+            thrower1.setVelocity(targetTps);
+            thrower2.setVelocity(targetTps);
+
+            double v1 = thrower1.getVelocity() / ShooterPIDConfig.TICKS_PER_REV * 60.0;
+            double v2 = thrower2.getVelocity() / ShooterPIDConfig.TICKS_PER_REV * 60.0;
+
+            hood.setPosition(hoodPos);
+
+            TelemetryPacket p = new TelemetryPacket();
+            p.put("target_tps", targetTps);
+            p.put("thrower1_tps", v1);
+            p.put("thrower2_tps", v2);
+            p.put("hood: ", hood.getPosition());
+            p.put("ll: ", ll.fetchFlywheelSpeed(limelight));
+            dashboard.sendTelemetryPacket(p);
+
+            telemetry.addData("Target (tps)", targetTps);
+            telemetry.addData("Thrower1 (tps)", v1);
+            telemetry.addData("Thrower2 (tps)", v2);
+            telemetry.addData("ll: ", ll.fetchFlywheelSpeed(limelight));
+            telemetry.addData("hood:", hood.getPosition());
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ShooterPIDConfig.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class ShooterPIDConfig {
+
+    // 6000 RPM goBILDA motors — they generally use 28 CPR encoders.
+    // If you have no gearbox, then ticksPerRev = 28.
+    // If you have a gearbox, do: 28 * gearRatio.
+    public static double TICKS_PER_REV = 28.0;
+
+    // These will be tunable in Dashboard
+    public static double kP = 100;
+    public static double kI = 0.0;
+    public static double kD = 0.0;
+    public static double kF = 0.000357;
+
+    // Desired shooter RPM (dashboard-adjustable)
+    public static double targetRpm = 0;
+}
