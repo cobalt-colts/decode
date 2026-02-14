5b12b6ec666894cccbe2c1c054664c6c8416c4db
ftc6547 <47903740+ftc6547@users.noreply.github.com>
2026-02-14 11:11:36 -0600

    Updated to use encoders w/ both motors and I

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/flywheeltuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/flywheeltuning.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 @TeleOp // Registers this OpMode as a TeleOp.
 public class flywheeltuning extends OpMode {
-    public DcMotorEx flywheelMotor;
+    public DcMotorEx thrower1;
     public DcMotorEx thrower2;
 
     FtcDashboard dashboard = FtcDashboard.getInstance();
@@ -24,6 +24,7 @@ public class flywheeltuning extends OpMode {
     // Initial PIDF coefficients for tuning.
     double F = 14.098; // Feedforward gain to counteract constant forces like friction.
     double P = 265;    // Proportional gain to correct error based on how far off the velocity is.
+    double I = 1.0;     // Not sure we really need this but <shrug> -Crocker
 
     // Array of step sizes for making fine or coarse adjustments to P and F.
     double[] stepSizes = {10.0, 1.0, 0.1, 0.001, 0.0001};
@@ -33,12 +34,14 @@ public class flywheeltuning extends OpMode {
 
     @Override
     public void init() {
-        flywheelMotor = hardwareMap.get(DcMotorEx.class, "thrower1");
+        thrower1 = hardwareMap.get(DcMotorEx.class, "thrower1");
         thrower2 = hardwareMap.get(DcMotorEx.class, "thrower2");
-        flywheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        thrower1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        thrower2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
-        flywheelMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, I, 0, F);
+        thrower1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        thrower2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
         telemetry.addLine("Init complete");
     }
 
@@ -75,17 +78,23 @@ public class flywheeltuning extends OpMode {
         }
 
 
-        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, I, 0, F);
         // Apply the new coefficients to the motor in every loop iteration.
-        flywheelMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        thrower1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        thrower2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
 
         // Command the motor to run at the current target velocity.
-        flywheelMotor.setVelocity(curTargetVelocity);
-        thrower2.setPower(flywheelMotor.getPower());
+        thrower1.setVelocity(curTargetVelocity);
+        thrower2.setVelocity(curTargetVelocity);
+//        if (thrower1.getPower() > 0.9) {
+//            thrower2.setPower(0.9);
+//        } else {
+//            thrower2.setPower(thrower1.getPower());
+//        }
 
         // --- Telemetry Output ---
 
-        double curVelocity = flywheelMotor.getVelocity();
+        double curVelocity = thrower1.getVelocity();
         double error = curTargetVelocity - curVelocity;
 
         telemetry.addData("Target Velocity", curTargetVelocity);
@@ -95,6 +104,8 @@ public class flywheeltuning extends OpMode {
         telemetry.addData("Tuning P", "%.4f (D-Pad U/D)", P);
         telemetry.addData("Tuning F", "%.4f (D-Pad L/R)", F);
         telemetry.addData("Step Size", "%.4f (B Button)", stepSizes[stepIndex]);
+        telemetry.addData("thrower1.getVelocity()", "%.4f", thrower1.getVelocity());
+        telemetry.addData("thrower2.getVelocity()", "%.4f", thrower2.getVelocity());
 
         TelemetryPacket packet = new TelemetryPacket();
         packet.put("Target Velocity", curTargetVelocity);
