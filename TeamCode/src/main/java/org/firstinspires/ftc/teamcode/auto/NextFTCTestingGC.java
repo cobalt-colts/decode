896eeaf52cf4315a35837a82a183c3769212d0f8
ftc6547 <47903740+ftc6547@users.noreply.github.com>
2026-03-03 15:35:57 -0600

    New test auto, stripped down to the basics to figure out why conditionals weren't working

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/NextFTCTestingGC.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import static org.firstinspires.ftc.teamcode.util.posConstants.flicker1down;
+import static org.firstinspires.ftc.teamcode.util.posConstants.flicker1up;
+import static org.firstinspires.ftc.teamcode.util.subsystems.isoccupied;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.util.paths.redgoalnine;
+import org.firstinspires.ftc.teamcode.util.posConstants;
+import org.firstinspires.ftc.teamcode.util.subsystems;
+import org.firstinspires.ftc.teamcode.util.subsystems.Index;
+import org.firstinspires.ftc.teamcode.util.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.util.subsystems.Thrower;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.conditionals.IfElseCommand;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.delays.WaitUntil;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.core.commands.utility.NullCommand;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.core.units.Angle;
+import dev.nextftc.extensions.pedro.FollowPath;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.extensions.pedro.TurnTo;
+import dev.nextftc.ftc.ActiveOpMode;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.hardware.positionable.SetPosition;
+
+import org.firstinspires.ftc.teamcode.util.SequentialGroupFixed;
+
+@Config
+@Autonomous(name = "NextFTCTestingGC", preselectTeleOp = "Sriram's ChatGPT TeleOp", group = "Red Goal")
+public class NextFTCTestingGC extends NextFTCOpMode {
+
+
+
+    public NextFTCTestingGC() throws InterruptedException {
+        addComponents(
+                new SubsystemComponent(Thrower.INSTANCE,
+                        Index.INSTANCE,
+                        Intake.INSTANCE,
+                        subsystems.Turret.INSTANCE,
+                        subsystems.Camera.INSTANCE,
+                        subsystems.ColorSensing.INSTANCE),
+                new PedroComponent(Constants::createFollower)
+
+//                BulkReadComponent.INSTANCE
+        );
+    }
+    private Command autoRoutine() {
+        return new SequentialGroupFixed(
+                new WaitUntil(() -> Thrower.INSTANCE.atvelocity),
+                Index.INSTANCE.flickerOrder,
+                Index.INSTANCE.sensedunsortedatspeed(),
+                Index.INSTANCE.sensedunsortedatspeed()  // In case one didn't launch, order doesn't matter just get it launched
+        );
+    }
+
+//    private Command autoRoutine_disabled() {
+//        return new SequentialGroupFixed(
+//                new WaitUntil(() -> Thrower.INSTANCE.atvelocity),
+//                Index.INSTANCE.flickerOrder,
+//                Index.INSTANCE.closesortedLaunch,
+//                Index.INSTANCE.closesortedLaunch
+////                Index.INSTANCE.sensedunsorted,
+////                Index.INSTANCE.sensedunsorted
+//        );
+//    }
+    @Override public void onInit() {
+        Index.INSTANCE.alldown.schedule();
+//        subsystems.Turret.initPos = posConstants.redGoalInit;
+//        subsystems.Turret.INSTANCE.redgoalinit.schedule();
+    }
+    @Override
+    public void onStartButtonPressed() {
+        subsystems.start = true;
+        redgoalnine.BuildTrajectories(PedroComponent.Companion.follower());
+        PedroComponent.Companion.follower().setStartingPose(new Pose(122, 126, Math.toRadians(-143.5)));
+        autoRoutine().schedule();
+    }
+    @Override public void onUpdate() { }
+    @Override public void onStop() {
+        subsystems.start = false;
+    }
+
+}
