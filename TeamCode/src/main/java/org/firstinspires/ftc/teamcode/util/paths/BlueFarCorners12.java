382b288fe488e5fd5e82ddc766cc042ced981667
ftc6547 <47903740+ftc6547@users.noreply.github.com>
2026-03-04 21:49:27 -0600

    Extensive path updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/paths/BlueFarCorners12.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/paths/BlueFarCorners12.java
@@ -22,10 +22,10 @@ public class BlueFarCorners12 {
         line1 = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
-                                new Pose(84.575, 9.208),
-                                new Pose(84.688, 41.458),
-                                new Pose(95.239, 35.667),
-                                new Pose(133.000, 35.500)
+                                new Pose(59.425, 9.208),
+                                new Pose(59.312, 41.458),
+                                new Pose(48.761, 34.833),
+                                new Pose(11.000, 35.500)
                         )
                 )
                 .setTangentHeadingInterpolation()
@@ -34,91 +34,89 @@ public class BlueFarCorners12 {
         launch1 = follower.pathBuilder()
                 .addPath(
                         new BezierLine(
-                                new Pose(133.000, 35.500),
-                                new Pose(90.000, 15.000)
+                                new Pose(11.000, 35.500),
+                                new Pose(54.000, 15.000)
                         )
                 )
-                .setConstantHeadingInterpolation(Math.toRadians(0))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
                 .build();
 
         corner1 = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
-                                new Pose(90.000, 15.000),
-                                new Pose(108.000, 15.000),
-                                new Pose(128.375, 44.253),
-                                new Pose(132.630, 14.918)
+                                new Pose(54.000, 15.000),
+                                new Pose(36.000, 15.000),
+                                new Pose(15.625, 44.253),
+                                new Pose(11.370, 14.918)
                         )
                 )
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(-45))
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(225))
                 .build();
 
         bump1 = follower.pathBuilder()
                 .addPath(
                         new BezierLine(
-                                new Pose(132.630, 14.918),
-                                new Pose(135.369, 8.783)
+                                new Pose(11.370, 14.918),
+                                new Pose(8.631, 8.783)
                         )
                 )
-                .setLinearHeadingInterpolation(Math.toRadians(-45), Math.toRadians(-90))
+                .setLinearHeadingInterpolation(Math.toRadians(225), Math.toRadians(270))
                 .build();
 
         launch2 = follower.pathBuilder()
                 .addPath(
-                        new BezierCurve(
-                                new Pose(135.369, 8.783),
-                                new Pose(121.812, 17.169),
-                                new Pose(102.255, 8.386)
+                        new BezierLine(
+                                new Pose(8.631, 8.783),
+                                new Pose(41.745, 8.386)
                         )
                 )
-                .setLinearHeadingInterpolation(Math.toRadians(-90), Math.toRadians(0))
+                .setConstantHeadingInterpolation(Math.toRadians(270))
                 .build();
 
         corner2 = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
-                                new Pose(102.255, 8.386),
-                                new Pose(102.725, 25.583),
-                                new Pose(132.107, 14.414),
-                                new Pose(131.754, 22.331),
-                                new Pose(134.744, 31.443)
-                        )
-                )
-                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(70))
-                .setReversed()
-                .build();
-
-        bump2 = follower.pathBuilder()
-                .addPath(
-                        new BezierCurve(
-                                new Pose(134.744, 31.443),
-                                new Pose(131.161, 20.802),
-                                new Pose(135.287, 13.635),
-                                new Pose(135.330, 8.961)
+                                new Pose(41.745, 8.386),
+                                new Pose(41.275, 25.583),
+                                new Pose(11.893, 14.414),
+                                new Pose(12.246, 22.331),
+                                new Pose(9.256, 31.443)
                         )
                 )
                 .setTangentHeadingInterpolation()
                 .setReversed()
                 .build();
 
+        bump2 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(9.256, 31.443),
+                                new Pose(12.839, 20.802),
+                                new Pose(8.713, 13.635),
+                                new Pose(8.670, 8.961)
+                        )
+                )
+                .setTangentHeadingInterpolation()
+                .build();
+
         launch3 = follower.pathBuilder()
                 .addPath(
                         new BezierLine(
-                                new Pose(135.330, 8.961),
-                                new Pose(102.111, 8.395)
+                                new Pose(8.670, 8.961),
+                                new Pose(41.889, 8.395)
                         )
                 )
-                .setConstantHeadingInterpolation(Math.toRadians(90))
+                .setConstantHeadingInterpolation(Math.toRadians(270))
                 .build();
 
         offline = follower.pathBuilder()
                 .addPath(
                         new BezierLine(
-                                new Pose(102.111, 8.395),
-                                new Pose(84.000, 36.000)
+                                new Pose(41.889, 8.395),
+                                new Pose(39.000, 33.000)
                         )
                 )
-                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(180))
+                .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(180))
                 .build();
     }
 }
